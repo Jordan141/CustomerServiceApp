b582336c96bed3c89c7ed4453927672bd0f07e7c
Jordan <jmoss636@gmail.com>
2017-09-02 18:04:14 +0100

    basic socket change

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
-const express = require('express'),
-app = express(),
-port = process.env.PORT || 3000
-
+const app = require('express')()
+const port = process.env.PORT || 3000
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'pug')
@@ -11,6 +9,14 @@ app.get('/', function(req,res){
     res.render('temp')
 })
 
-app.listen(port)
+const io = require('socket.io').listen(app.listen(port))
+
+io.on('connection', function(socket){
+    socket.emit('message', {message: 'Welcome to the chat!'})
+    socket.on('send', function(data){
+        io.sockets.emit('message', data)
+    })
+})
+
 
 console.log('Listening on port: ' + port)
